db2ed149ed8c0ec7b909835a9db0e63d2e617ec6
karukaaa <arukhanym.zhaidary@gmail.com>
2025-10-16 00:52:29 +0500

    search works but debounce is weird

--- src/app/jewelry-list/jewelry-list.ts
+++ src/app/jewelry-list/jewelry-list.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Jewelry, JewelryService } from '../services/jewelry-service';
 import { CommonModule } from '@angular/common';
+import { debounceTime, distinctUntilChanged, of, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-jewelry-list',
@@ -9,22 +10,53 @@ import { CommonModule } from '@angular/common';
   templateUrl: './jewelry-list.html',
   styleUrl: './jewelry-list.css',
 })
+
+//this is branch live search
 export class JewelryList implements OnInit {
-  jewelryList: Jewelry[] = [];
   isLoaded = false;
+  private searchTerms = new Subject<string>();
+  jewelryList: Jewelry[] = [];
+  allJewelry: Jewelry[] = [];
 
   constructor(private jewelryService: JewelryService) {}
-  //this is branch live search
+
   ngOnInit(): void {
     this.jewelryService.getJewelry().subscribe({
       next: (data) => {
+        this.allJewelry = data;
         this.jewelryList = data;
-        console.log(data);
       },
+      error: (err) => console.log(err),
     });
+
+    this.searchTerms
+      .pipe(
+        debounceTime(200),
+        switchMap((term) => {
+          const trimmed = term.trim().toLowerCase();
+
+          if (trimmed === '') {
+            return of(this.allJewelry);
+          }
+
+          return of(
+            this.allJewelry.filter((item) =>
+              (item.description + item.name).toLowerCase().includes(trimmed)
+            )
+          );
+        })
+      )
+      .subscribe((result) => {
+        this.jewelryList = result;
+      });
   }
 
   loadJewelry(): void {
+    this.jewelryList = this.allJewelry;
     this.isLoaded = true;
+  }
+
+  onSearch(query: string): void {
+    this.searchTerms.next(query);
   }
 }
--- src/app/services/jewelry-service.ts
+++ src/app/services/jewelry-service.ts
@@ -2,25 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class JewelryService {
-  apiUrl = "https://my-json-server.typicode.com/karukaaa/AngularAruriApi/jewelry";
+  apiUrl = 'https://my-json-server.typicode.com/karukaaa/AngularAruriApi/jewelry';
 
-  constructor (private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  public getJewelry(): Observable<Jewelry[]>{
+  public getJewelry(): Observable<Jewelry[]> {
     return this.http.get<Jewelry[]>(this.apiUrl);
+  }
+
+  public searchJewelry(query: string): Observable<Jewelry[]> {
+    return this.http.get<Jewelry[]>(`${this.apiUrl}?q=${query}`);
   }
 }
 
-export interface Jewelry{
+export interface Jewelry {
   id: number;
   name: string;
   description: string;
 }
-
